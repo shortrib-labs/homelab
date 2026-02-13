acf535e46f438ffad2ea7264e95a7e3377b3c212
Chuck D'Antonio <chuck@crdant.io>
2026-02-13 14:42:44 +0000

    Prepares repository for GitOps
    TL;DR
    -----
    
    Scaffolds a the repository for GitOps with FluxCD
    
    Details
    -------
    
    Provides the tooling required to work with this repository that
    will become a monorepo for applications installed across my fleet
    of Homelab clusters. Uses a Nix flake and a dev shell configuration
    to make sure the require tools are available.
    
    The tools provide an indication of the overall plan for the
    environment. We install Flux for the overall GitOps engine, SOPS
    for secrets, and the pair of Velero and Restic for backups. Age
    will provide the SOPS keys, and all the regular Kubernetes goodies
    are included.

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Homelab Kubernetes management environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, devshell }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlays.default ];
+        };
+      in
+      {
+        devShells.default = pkgs.devshell.mkShell {
+          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+        };
+      }
+    );
+}
